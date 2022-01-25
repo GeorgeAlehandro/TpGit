632ade97b08a52f1b41cd8ee221f5c5cd7d947e9
George Alehandro Saad <74742818+GeorgeAlehandro@users.noreply.github.com>
2022-01-25 09:42:16 +0100

    Add files via upload

---
+++ TP3/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    main.py is the main program that calls for adn.py functions to verify the 
+    dna chains input
+"""
+from adn import *
+if __name__ == "__main__":  
+    get_valid_adn()
+    
+
+
+
