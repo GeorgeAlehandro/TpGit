cc426c36645529dcc8675e9366a61c5584e585e0
George Alehandro Saad <74742818+GeorgeAlehandro@users.noreply.github.com>
2022-02-19 11:06:40 +0100

    Update view_obj.py

--- TP6/view_obj.py
+++ TP6/view_obj.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 import os
+from view import SuperView
 from tkinter import Button
 from tkinter import Entry
 from tkinter import filedialog
@@ -12,6 +13,8 @@ from tkinter import Toplevel
 from tkinter import messagebox
 from tkinter import ttk
 from tkinter.ttk import Treeview
+from ttkwidgets.autocomplete import AutocompleteEntry
+from autocomplete import AutocompleteEntry
 """
 Class View of the project
 
@@ -19,15 +22,14 @@ Class View of the project
 """
 
 
-class View(Tk):
+class View(Tk, SuperView):
     def __init__(self, controller):
-        super().__init__()
-        self.controller = controller
+        super().__init__(controller)
         self.widgets_labs = {}
         self.widgets_entry = {}
         self.widgets_button = {}
-        self.entries = ["Nom", "Prenom", "Telephone", "Adresse", "Ville"]
-        self.buttons = ["Chercher", "Inserer", "Effacer", "Display"]
+        self.buttons = ["Chercher", "Inserer", "Remove"]
+        self.extrabuttons = ["Content", "Clear"]
         self.modelListFields = []
         self.fileName = None
         self.windows = {}
@@ -35,16 +37,20 @@ class View(Tk):
         self.windows["fenetreErreur"] = ...
 
     def get_value(self):
-        entry_fetch = self.widgets_entry["Nom"].get(), self.widgets_entry["Prenom"].get(
-        ), self.widgets_entry["Telephone"].get(), self.widgets_entry["Adresse"].get(), self.widgets_entry["Ville"].get()
+        entry_fetch = self.widgets_entry["Surname"].get(), self.widgets_entry["Name"].get(
+        ), self.widgets_entry["Telephone"].get(), self.widgets_entry["Address"].get(), self.widgets_entry["City"].get()
         if all(item == '' for item in entry_fetch):
             messagebox.showerror('Entry unavailable',
                                  'Entries cannot be all empty.')
             return False
+        if not self.widgets_entry["Telephone"].get().isdigit() and self.widgets_entry["Telephone"].get() != '':
+            messagebox.showerror(
+                'Insertion error', "Telephone should be only digits.")
+            return False
         return entry_fetch
 
     def create_fields(self):
-        i, j = 0, 0
+        i, j, h = 0, 0, 0
 
         for idi in self.entries:
             lab = Label(self, text=idi.capitalize())
@@ -60,18 +66,33 @@ class View(Tk):
 
         for idi in self.buttons:
             buttonW = Button(self, text=idi, command=(
-                lambda button=idi: self.controller.button_press_handle(button)))
+                lambda button=idi: self.button_press_handle(button)))
             self.widgets_button[idi] = buttonW
             buttonW.grid(row=i+1, column=j)
             # self.widgets_button[idi].config(command = idi)
 
             j += 1
+        for idi in self.extrabuttons:
+            buttonW = Button(self, text=idi, command=(
+                lambda button=idi: self.button_press_handle(button)))
+            self.widgets_button[idi] = buttonW
+            buttonW.grid(row=h, column=2, rowspan=2)
+            # self.widgets_button[idi].config(command = idi)
 
-    def result_display(self, title, result):
+            h += 2
+
+    def delete_display(self, title, result):
         if result is not None:
             messagebox.showinfo(title, result)
         else:
-            messagebox.showerror(title, 'not found')
+            messagebox.showerror(title, 'Surname Name not found in phonebook.')
+
+    def insertion_display(self, title, result):
+        if result is not None:
+            messagebox.showinfo(title, result)
+        else:
+            messagebox.showerror(
+                title, 'You need to specify name and surname.')
 
     def on_closing(self):
         message_closing = "Do you want to quit? Modifications will be saved."
@@ -80,38 +101,54 @@ class View(Tk):
             self.destroy()
 
     def result_presentation(self, items_list):
-        treepresentation = Toplevel(self)
-        treepresentation.title("Addresse Book")
-        columns = ("Name", "Prenom", "Telephone", "Adresse", "Ville")
-        tree = Treeview(treepresentation, columns=columns, show='headings')
-        tree.heading("Name", text="Name")
-        tree.heading("Prenom", text="Prenom")
-        tree.heading("Telephone", text="Telephone")
-        tree.heading("Adresse", text="Adresse")
-        tree.heading("Ville", text="Ville")
+        if items_list is not None:
+            treepresentation = Toplevel(self)
+            treepresentation.title("Address Book")
+            columns = ("Surname", "Name", "Telephone", "Address", "City")
+            tree = Treeview(treepresentation, columns=columns, show='headings')
+            tree.heading("Surname", text="Surname")
+            tree.heading("Name", text="Name")
+            tree.heading("Telephone", text="Telephone")
+            tree.heading("Address", text="Address")
+            tree.heading("City", text="City")
 
-        for person in items_list:
-            tree.insert("", "end", values=(
-                person['name'], person['prenom'], person['telephone'], person['adresse'], person['ville']))
+            for person in items_list:
+                tree.insert("", "end", values=(
+                    person['surname'], person['name'], person['telephone'],
+                    person['address'], person['city']))
 
-        tree.grid(row=0, column=0, sticky='nsew')
+            tree.grid(row=0, column=0, sticky='nsew')
 
-        def item_selected(event):
-            for selected_item in tree.selection():
-                item = tree.item(selected_item)
-                record = item['values']
-                print(record)
-                messagebox.showinfo(title='Information',
-                                    message=record, parent=treepresentation)
-        tree.bind('<<TreeviewSelect>>', item_selected)
-        quit_button = Button(treepresentation,  text="Quit",
-                             command=lambda: treepresentation.destroy())
-        quit_button.grid(row=1, column=0, sticky='s')
+            def item_selected(event):
+                for selected_item in tree.selection():
+                    item = tree.item(selected_item)
+                    record = item['values']
+                    print(record)
+                    messagebox.showinfo(title='Information',
+                                        message=record, parent=treepresentation)
+            tree.bind('<<TreeviewSelect>>', item_selected)
+            quit_button = Button(treepresentation,  text="Quit",
+                                 command=lambda: treepresentation.destroy())
+            quit_button.grid(row=1, column=0, sticky='s')
+        else:
+            messagebox.showerror('Results', 'Results were not found')
+
+    def button_press_handle(self, buttonId):
+        if buttonId == "Chercher":
+            self.controller.search()
+        elif buttonId == "Remove":
+            self.controller.delete()
+        elif buttonId == "Inserer":
+            self.controller.insert()
+        elif buttonId == "Content":
+            self.controller.display()
+        elif buttonId == "Clear":
+            for value in self.widgets_entry.values():
+                value.delete(0, 'end')
 
     def main(self):
         print("[View] main")
         self.title("Annuaire")
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
-        self.mainloop()
-        self.mainloop()
+        self.create_fields()
         self.mainloop()
