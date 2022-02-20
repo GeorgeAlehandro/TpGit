bc34718e47289dd84d397bca91cb3479d0315e4a
George Alehandro Saad <74742818+GeorgeAlehandro@users.noreply.github.com>
2022-02-20 23:44:48 +0100

    adding new files, need modification

---
+++ TP6/tkinter_run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 19 00:56:13 2022
+
+@author: ubuntu
+"""
+import os
+os.system(
+    "gnome-terminal -e 'bash -c \"python3 main.py -g; exec bash\"'")
---
+++ TP6/view.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+"""
+SuperView
+
+"""
+
+
+class SuperView():
+    def __init__(self, controller):
+        self.controller = controller
+        self.entries = ["Surname", "Name", "Telephone", "Address", "City"]
+
+    def create_interface(self):
+        pass
+
+    def get_value(self):
+        pass
+
+    def delete_display(self, title, result):
+        pass
+
+    def insertion_display(self, title, result):
+        pass
+
+    def result_presentation(self, items_list):
+        pass
+
+    def main(self):
+        pass
+
+# TODO FIND A SIMPLER STRUCTURE OF ARGUMENTS, SHI ENNO BYE2BAL GET VALUE MASALAN
+# TODO FIND A SIMPLER STRUCTURE OF ARGUMENTS, SHI ENNO BYE2BAL GET VALUE MASALAN
