39250e74864d885cffcf48363858aceca6f93a59
George Alehandro Saad <74742818+GeorgeAlehandro@users.noreply.github.com>
2022-02-20 23:31:42 +0100

    Update view_obj.py

--- TP6/view_obj.py
+++ TP6/view_obj.py
@@ -1,94 +1,130 @@
 # coding: utf-8
+"""
+Tkinter View of the project.
 
-import os
-from view import SuperView
+"""
+
+from __future__ import absolute_import
 from tkinter import Button
-from tkinter import Entry
-from tkinter import filedialog
 from tkinter import Label
-from tkinter import Menu
-from tkinter import StringVar
 from tkinter import Tk
 from tkinter import Toplevel
 from tkinter import messagebox
-from tkinter import ttk
 from tkinter.ttk import Treeview
 from ttkwidgets.autocomplete import AutocompleteEntry
-from autocomplete import AutocompleteEntry
-"""
-Class View of the project
-
-@author : Olivier CHABROL
-"""
+from view import SuperView
 
 
 class View(Tk, SuperView):
+    '''
+    Class for the Tkinter view (inherits from SuperView)
+    '''
+
     def __init__(self, controller):
+        '''
+        Constructor for the Tkinter view.
+        '''
         super().__init__()
         SuperView.__init__(self, controller)
         self.widgets_labs = {}
         self.widgets_entry = {}
         self.widgets_button = {}
-        self.buttons = ["Chercher", "Inserer", "Remove"]
+        self.buttons = ["Search", "Insert", "Remove"]
         self.extrabuttons = ["Content", "Clear"]
-        self.modelListFields = []
-        self.fileName = None
-        self.windows = {}
-        self.windows["fenetreResult"] = ...
-        self.windows["fenetreErreur"] = ...
+        self.autocomplete_values = []
+
+    def fetch_autocomplete_values(self):
+        '''
+        Returns a list containing the elements to be used in the autocompletion
+        entry. Elements are inside the memory.
+        '''
+        (list_surname, list_name,
+         list_telephone, list_address,
+         list_city) = self.controller.memory_generator()
+        self.autocomplete_values = [list_surname, list_name,
+                                    list_telephone,
+                                    list_address, list_city]
+        print(self.autocomplete_values)
 
     def get_value(self):
-        entry_fetch = self.widgets_entry["Surname"].get(), self.widgets_entry["Name"].get(
-        ), self.widgets_entry["Telephone"].get(), self.widgets_entry["Address"].get(), self.widgets_entry["City"].get()
+        '''
+        Returns the values introduced by the user.
+        '''
+        entry_fetch = (self.widgets_entry["Surname"].get(),
+                       self.widgets_entry["Name"].get(
+        ), self.widgets_entry["Telephone"].get(),
+            self.widgets_entry["Address"].get(),
+            self.widgets_entry["City"].get())
         if all(item == '' for item in entry_fetch):
             messagebox.showerror('Entry unavailable',
                                  'Entries cannot be all empty.')
             return False
-        if not self.widgets_entry["Telephone"].get().isdigit() and self.widgets_entry["Telephone"].get() != '':
+        if (not self.widgets_entry["Telephone"].get().isdigit() and
+                self.widgets_entry["Telephone"].get() != ''):
             messagebox.showerror(
                 'Insertion error', "Telephone should be only digits.")
             return False
+        # self.list_surname.append(entry_fetch[0])
+        # self.list_name.append(entry_fetch[1])
+        # self.list_telephone.append(entry_fetch[2])
+        # self.list_address.append(entry_fetch[3])
+        # self.list_city.append(entry_fetch[4])
+        # self.fetch_autocomplete_values()
         return entry_fetch
 
     def create_fields(self):
-        i, j, h = 0, 0, 0
+        '''
+        Creates the different elements of the graphical interface.
+        '''
+        i, j, k = 0, 0, 0
 
         for idi in self.entries:
             lab = Label(self, text=idi.capitalize())
             self.widgets_labs[idi] = lab
             lab.grid(row=i, column=0)
+            entry = AutocompleteEntry(
+                self,
+                width=30,
+                font=('Times', 12),
+                completevalues=self.autocomplete_values[i]
 
-            var = StringVar()
-            entry = Entry(self, text=var)
+            )
+            print(self.autocomplete_values[i])
             self.widgets_entry[idi] = entry
             entry.grid(row=i, column=1)
 
             i += 1
 
         for idi in self.buttons:
-            buttonW = Button(self, text=idi, command=(
+            buttown = Button(self, text=idi, command=(
                 lambda button=idi: self.button_press_handle(button)))
-            self.widgets_button[idi] = buttonW
-            buttonW.grid(row=i+1, column=j)
+            self.widgets_button[idi] = buttown
+            buttown.grid(row=i+1, column=j)
             # self.widgets_button[idi].config(command = idi)
 
             j += 1
         for idi in self.extrabuttons:
-            buttonW = Button(self, text=idi, command=(
+            buttown = Button(self, text=idi, command=(
                 lambda button=idi: self.button_press_handle(button)))
-            self.widgets_button[idi] = buttonW
-            buttonW.grid(row=h, column=2, rowspan=2)
+            self.widgets_button[idi] = buttown
+            buttown.grid(row=k, column=2, rowspan=2)
             # self.widgets_button[idi].config(command = idi)
 
-            h += 2
+            k += 2
 
     def delete_display(self, title, result):
+        '''
+        Message display after delete.
+        '''
         if result is not None:
             messagebox.showinfo(title, result)
         else:
             messagebox.showerror(title, 'Surname Name not found in phonebook.')
 
     def insertion_display(self, title, result):
+        '''
+        Message display after insertion
+        '''
         if result is not None:
             messagebox.showinfo(title, result)
         else:
@@ -96,12 +132,18 @@ class View(Tk, SuperView):
                 title, 'You need to specify name and surname.')
 
     def on_closing(self):
+        '''
+        Function that sets the behavior when closing the graphical interface.
+        '''
         message_closing = "Do you want to quit? Modifications will be saved."
         if messagebox.askokcancel("Quit", message_closing):
             self.controller.save_notebook()
             self.destroy()
 
     def result_presentation(self, items_list):
+        '''
+        Results presentation after fetching.
+        '''
         if items_list is not None:
             treepresentation = Toplevel(self)
             treepresentation.title("Address Book")
@@ -134,22 +176,28 @@ class View(Tk, SuperView):
         else:
             messagebox.showerror('Results', 'Results were not found')
 
-    def button_press_handle(self, buttonId):
-        if buttonId == "Chercher":
+    def button_press_handle(self, buttonid):
+        '''
+        Assign a command for each pressed button.
+        '''
+        if buttonid == "Search":
             self.controller.search()
-        elif buttonId == "Remove":
+        elif buttonid == "Remove":
             self.controller.delete()
-        elif buttonId == "Inserer":
+        elif buttonid == "Insert":
             self.controller.insert()
-        elif buttonId == "Content":
+        elif buttonid == "Content":
             self.controller.display()
-        elif buttonId == "Clear":
+        elif buttonid == "Clear":
             for value in self.widgets_entry.values():
                 value.delete(0, 'end')
 
     def main(self):
-        print("[View] main")
-        self.title("Annuaire")
+        '''
+        Main execution of the Tkinter view.
+        '''
+        self.title("Phone Notebook")
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.fetch_autocomplete_values()
         self.create_fields()
         self.mainloop()
