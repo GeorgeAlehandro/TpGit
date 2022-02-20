83a7e4507f7d90e15166c69fb885c1c7e406a52b
George Alehandro Saad <74742818+GeorgeAlehandro@users.noreply.github.com>
2022-02-20 23:32:48 +0100

    Update controller.py

--- TP6/controller.py
+++ TP6/controller.py
@@ -1,58 +1,106 @@
-from view_obj import View
+"""
+    controller.py makes the controller part of the MVC model for the phonebook
+    homework
+"""
+from __future__ import absolute_import
+import view_obj
+import view_cli
 from model import Ensemble
 from model import Person
 
+__author__ = 'George Alehandro Saad'
+
 
 class Controller():
+    '''
+    Defining the controller class and its methods.
+    '''
+
     def __init__(self):
-        self.view = View(self)
+        '''
+        Controller class constructor.
+        '''
+        self.view = view_cli.View(self)
         self.model = Ensemble()
 
     def start_view(self):
-        self.view.create_fields()
+        '''
+        Calls for the view interface.
+        '''
         self.view.main()
 
+    # def view_choice(self, choice):
+    #     if choice == 1:
+    #         self.view = view_obj.View(self)
+
+    #         self.view = view_cli.View(self)
+    #     self.start_view()
+    def switch_view(self):
+        '''
+        Switches from the CLI view into graphical one (Tkinter).
+        '''
+        self.view = view_obj.View(self)
+        self.memory_generator()
+        self.start_view()
+
     def search(self):
+        '''
+        Processes the search request of a person inside the notebook.
+        '''
         values_unpack = self.view.get_value()
         if values_unpack:
-            a = self.model.search_person(values_unpack)
-            self.view.result_display('Found', a)
-            self.view.result_presentation(a)
+            fetching = self.model.search_person(values_unpack)
+            # if fetching is not None:
+            self.view.result_presentation(fetching)
 
     def delete(self):
+        '''
+        Processes the removal request of a person inside the notebook.
+        '''
         values_unpack = self.view.get_value()
         if values_unpack:
             deleted = self.model.delete_person(values_unpack)
-            self.view.result_display('Deleted', deleted)
+            self.view.delete_display('Deleted', deleted)
 
     def insert(self):
+        '''
+        Processes the insert request of a person inside the notebook.
+        '''
         values_unpack = self.view.get_value()
-        person = Person(*values_unpack)
-        inserted = self.model.insert_person(person)
-        self.view.result_display('Successfuly inserted', inserted)
+        if values_unpack:
+            person = Person(*values_unpack)
+            inserted = self.model.insert_person(person)
+            self.view.insertion_display('Insertion', inserted)
 
     def save_notebook(self):
-        print('Tried to save.')
+        '''
+        Controller call for saving the model.
+        '''
+        print('Progress saved.')
         self.model.save()
 
     def display(self):
+        '''
+        Displays the content of the notebook.
+        '''
         contain = self.model.display_all()
         self.view.result_presentation(contain)
 
-    def button_press_handle(self, buttonId):
-        print("[Controller][button_press_handle] " + buttonId)
-        if buttonId == "Chercher":
-            self.search()
-        elif buttonId == "Effacer":
-            self.delete()
-        elif buttonId == "Inserer":
-            self.insert()
-        elif buttonId == "Display":
-            self.display()
-        else:
-            pass
-
-
-if __name__ == "__main__":
-    controller = Controller()
-    controller.start_view()
+    def memory_generator(self):
+        '''
+        Returns a list for each that will then be used for the
+        autcompleteEntry
+        '''
+        list_name = []
+        list_surname = []
+        list_telephone = []
+        list_address = []
+        list_city = []
+        contain = self.model.display_all()
+        for item in contain:
+            list_surname.append(item['surname'])
+            list_name.append(item['name'])
+            list_telephone.append(item['telephone'])
+            list_address.append(item['address'])
+            list_city.append(item['city'])
+        return list_surname, list_name, list_telephone, list_address, list_city
