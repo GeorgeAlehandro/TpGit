f4e45d3ac3497ed6f7270033845b7b3e26a61ba0
George Alehandro Saad <74742818+GeorgeAlehandro@users.noreply.github.com>
2022-02-22 11:07:14 +0100

    Update view.py

--- TP6/view.py
+++ TP6/view.py
@@ -1,32 +1,44 @@
 # coding: utf-8
 """
-SuperView
+SuperView of the MVC model
 
 """
 
 
 class SuperView():
+    '''
+    Super class of both views.
+    '''
+
     def __init__(self, controller):
+        '''
+        constructor of the super class.
+        '''
         self.controller = controller
         self.entries = ["Surname", "Name", "Telephone", "Address", "City"]
+        self.error_messages = ["You need to specify name and surname.",
+                               "Surname Name not found in phonebook.",
+                               "Telephone should be only digits.",
+                               'Entries cannot be all empty.'
+                               ]
 
-    def create_interface(self):
-        pass
+    # def create_interface(self):
+    #     pass
 
-    def get_value(self):
-        pass
+    # def get_value(self):
+    #     pass
 
-    def delete_display(self, title, result):
-        pass
+    # def delete_display(self, title, result):
+    #     pass
 
-    def insertion_display(self, title, result):
-        pass
+    # def insertion_display(self, title, result):
+    #     pass
 
-    def result_presentation(self, items_list):
-        pass
+    # def result_presentation(self, items_list):
+    #     '''
+    #     Results presentation after fetching.
+    #     '''
+    #     pass
 
-    def main(self):
-        pass
-
-# TODO FIND A SIMPLER STRUCTURE OF ARGUMENTS, SHI ENNO BYE2BAL GET VALUE MASALAN
-# TODO FIND A SIMPLER STRUCTURE OF ARGUMENTS, SHI ENNO BYE2BAL GET VALUE MASALAN
+    # def main(self):
+    #     pass
