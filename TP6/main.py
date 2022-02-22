c880b174f36627128b06ff03c6a761889da74fd5
George Alehandro Saad <74742818+GeorgeAlehandro@users.noreply.github.com>
2022-02-22 23:48:29 +0100

    Update main.py

--- TP6/main.py
+++ TP6/main.py
@@ -4,7 +4,7 @@
 The model file for the MVC structure of the phonebook program.
 """
 from controller import Controller
-import os
+#import os
 
 if __name__ == "__main__":
     controller = Controller()
